47f52268e2fe526f1852a5189708b84100618033
夏许强 <xiaxuqiang@changingedu.com>
2023-09-04 16:31:45 +0800

    测试工程

--- TestLibProject/NameClashesFinderVC.m
+++ TestLibProject/NameClashesFinderVC.m
@@ -21,17 +21,17 @@
     // Do any additional setup after loading the view from its nib.
     
     // DLIntrospection
-//    NSArray *classes = [NameClashesFinder classes];
-//    NSLog (@"classes: %@", classes);
+    NSArray *classes = [NameClashesFinderVC classes];
+    NSLog (@"classes: %@", classes);
     
     Class toCheckCls = [NameClashesFinderVC class];
     //Class toCheckCls = [NSObject class];
     
-//    NSArray *properties = [toCheckCls properties];
-//    NSLog (@"properties: %@", properties);
-//    
-//    NSArray *instanceVariables = [toCheckCls instanceVariables];
-//    NSLog (@"instanceVariables: %@", instanceVariables);
+    NSArray *properties = [toCheckCls properties];
+    NSLog (@"properties: %@", properties);
+    
+    NSArray *instanceVariables = [toCheckCls instanceVariables];
+    NSLog (@"instanceVariables: %@", instanceVariables);
     
     NSArray *classMethods = [toCheckCls classMethods];
     //NSLog (@"classMethods: %@", classMethods);
@@ -43,41 +43,42 @@
     NSLog (@"重复的实例方法: ");
     [self showDuplicateMethodStringWithArray:instanceMethods];
     
-//    NSArray *protocols = [toCheckCls protocols];
-//    NSLog (@"protocols: %@", protocols);
-//    
-//    NSString *parentClassHierarchy = [toCheckCls parentClassHierarchy];
-//    NSLog (@"parentClassHierarchy: %@", parentClassHierarchy);
+    NSArray *protocols = [toCheckCls protocols];
+    NSLog (@"protocols: %@", protocols);
+    
+    NSString *parentClassHierarchy = [toCheckCls parentClassHierarchy];
+    NSLog (@"parentClassHierarchy: %@", parentClassHierarchy);
     
     
-//    NSLog(@"获取所有加载的objective-c框架和动态库的名称");
-//    unsigned int allOutCount = 0;
-//    const char ** allImageNames = objc_copyImageNames(&allOutCount);
-//    
-//    const char *testLibImage = nil;
-//    for (int i = 0; i < allOutCount; i++) {
-//        const char *imageName = allImageNames[i];
-//        char *isCompare = strstr(imageName, "TestLibProject");
-//        if (isCompare) {
-//            testLibImage = imageName;
-//            break;
-//        }
-//    }
-//    
-//    NSLog (@"找到TestLibProject了: %s", testLibImage);
-//
-////    NSLog(@"获取指定类所在动态库");
-////    NSLog(@"UIView's Framework: %s", class_getImageName(NSClassFromString(@"UIView")));
-//
-//    NSLog(@"获取指定库或框架中所有类的类名");
-//    unsigned int outCount = 0;
-//    testLibImage = class_getImageName(NSClassFromString(@"NSObject"));
-//    NSLog (@"NSObject's Framework:: %s", testLibImage);
-//    //const char ** classes = objc_copyClassNamesForImage(class_getImageName(NSClassFromString(@"UIView")), &outCount);
-//    const char **classes = objc_copyClassNamesForImage(testLibImage, &outCount);
-//    for (int i = 0; i < outCount; i++) {
-//        NSLog(@"class name: %s", classes[i]);
-//    }
+    NSLog(@"获取所有加载的objective-c框架和动态库的名称");
+    unsigned int allOutCount = 0;
+    const char ** allImageNames = objc_copyImageNames(&allOutCount);
+    
+    const char *testLibImage = nil;
+    for (int i = 0; i < allOutCount; i++) {
+        const char *imageName = allImageNames[i];
+        printf("%s\n", imageName);
+        char *isCompare = strstr(imageName, "TestLibProject");
+        if (isCompare) {
+            testLibImage = imageName;
+            break;
+        }
+    }
+    
+    NSLog (@"找到TestLibProject了: %s", testLibImage);
+
+    NSLog(@"获取指定类所在动态库");
+    NSLog(@"UIView's Framework: %s", class_getImageName(NSClassFromString(@"UIView")));
+
+    NSLog(@"获取指定库或框架中所有类的类名");
+    unsigned int outCount = 0;
+    testLibImage = class_getImageName(NSClassFromString(@"NSObject"));
+    NSLog (@"NSObject's Framework:: %s", testLibImage);
+    //const char ** classes = objc_copyClassNamesForImage(class_getImageName(NSClassFromString(@"UIView")), &outCount);
+    const char **xxxclasses = objc_copyClassNamesForImage(testLibImage, &outCount);
+    for (int i = 0; i < outCount; i++) {
+        NSLog(@"class name: %s", xxxclasses[i]);
+    }
 }
 
 - (void)showDuplicateMethodStringWithArray:(NSArray *)methodStringArray {
